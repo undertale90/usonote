d54eafb2c95c608607f81d2c031720a003735664
shado <snowpaws70@gmail.com>
2018-07-31 04:18:29 -0500

    Update Flipnoteplayer.py

--- Flipnote-Player/Flipnoteplayer.py
+++ Flipnote-Player/Flipnoteplayer.py
@@ -1,2 +1,9 @@
 #Flipnote Player by pbsds
+
 DEBUG= False
+
+import pygame, sys, numpy as np, ConfigParser, time, os, glob, urlib2,random
+from scikits.samplerate import resample as SciResample
+from HatenaTools import PPM, UGO,NFTF
+
+if "win" in sas
